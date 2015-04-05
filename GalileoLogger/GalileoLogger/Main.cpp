746b2b67f3f6901e7b197231e5004b827071a5e4
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-04-05 10:22:30 +0530

    Added Galileo logging project

---
+++ GalileoLogger/GalileoLogger/Main.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "arduino.h"
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+    return RunArduinoSketch();
+}
+
+void setup()
+{
+    
+}
+
+// the loop routine runs over and over again forever:
+void loop()
+{
+    
+}
