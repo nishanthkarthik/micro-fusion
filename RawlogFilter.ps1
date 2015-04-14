d9ea80438c2c56d65f79f415c6371ad4a1ecb573
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-04-14 18:39:26 +0530

    Added fifo filter script

---
+++ RawlogFilter.ps1
@@ -0,0 +1,28 @@
+﻿$filepath = Read-Host 'Enter the location of file';
+$targetfilepath = Read-Host 'Enter the path for result (with backslash at end)'
+
+$writer = [System.IO.StreamWriter] (($targetfilepath + 'SerialLogResult'))
+
+$inarray = [IO.File]::ReadAllLines($filepath);
+[System.Collections.ArrayList] $inarraymod = $inarray;
+
+for ($i = 0; $i -le $inarraymod.Count; ++$i)
+{
+    if ($inarraymod[$i] -ne $null)
+    {
+        if ($inarraymod[$i].Equals("FIFO overflow!"))
+        {
+            echo ('Position ' + $i.ToString())
+            $inarraymod.RemoveAt($i);
+            $inarraymod.RemoveAt($i-1);
+        }
+    }
+}
+
+for ($j = 0; $j -le $inarraymod.Count; ++$j)
+{
+    
+}
+
+$writer.Close();
+echo 'Writer closed'
