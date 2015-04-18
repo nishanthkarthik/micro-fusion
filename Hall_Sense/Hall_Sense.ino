8af2a0a6ff1db4862c507a84564621ea9b51945f
Karthik Nishanth Sengottuvelavan <nishanthkarthik@live.com>
2015-04-18 18:29:16 +0530

    Analog sensor

--- Hall_Sense/Hall_Sense.ino
+++ Hall_Sense/Hall_Sense.ino
@@ -1,20 +1,33 @@
 #define LINE_COUNT 50
-#include <PinChangeInt.h>
+#define HALL_SENSOR A4
+
+int oldPinValue;
+int nowPinValue;
+long oldTimeStamp;
+long nowTimeStamp;
 
 void setup() 
 {
   Serial.begin(115200);
-  
+  oldTimeStamp = millis();
+  oldPinValue = returnLineCount(analogRead(HALL_SENSOR));
 }
 
 void loop() 
 {
-  printLines(returnLineCount(analogRead(A4)));
+  nowPinValue = returnLineCount(analogRead(HALL_SENSOR));
+  if (nowPinValue - oldPinValue >= 2)
+  {
+    nowTimeStamp = millis();
+    Serial.println(nowTimeStamp - oldTimeStamp);
+    oldTimeStamp = nowTimeStamp;
+  }
+  oldPinValue = nowPinValue;
 }
 
 int returnLineCount(int value)
 {
-  return (float)value / 1024 * LINE_COUNT;
+  return (float)value / 255 * LINE_COUNT;
 }
 
 void printLines(int value)
